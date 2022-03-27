7e9b7b2e92771e54af8574f095e30712971f7470
Nay Lin Aung <naylinaung.nla.dev@gmail.com>
2022-03-27 14:21:32 +0630

    ultrasonic and Blynk server

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,3 +1,22 @@
+#define BLYNK_PRINT Serial
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+#define BLYNK_TEMPLATE_ID "TMPLiFW1YUQe"
+#define BLYNK_DEVICE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "Rehdlp0-5g-FfSr6um4Lm162F_hRmlhT"
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "Rehdlp0-5g-FfSr6um4Lm162F_hRmlhT";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ProLab";
+char pass[] = "myanmar@@@";
+
 const int trigPin = 12;
 const int echoPin = 14;
 
@@ -13,6 +32,7 @@ void setup() {
   Serial.begin(115200); // Starts the serial communication
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Blynk.begin(auth, ssid, pass);
 }
 
 void loop() {
@@ -38,6 +58,11 @@ void loop() {
   Serial.println(distanceCm);
   Serial.print("Distance (inch): ");
   Serial.println(distanceInch);
+
+  Serial.println("Sending to IOT server ...");
+  Blynk.virtualWrite(V1,distanceCm);
+  Blynk.virtualWrite(V2,distanceInch);
+  
   
   delay(1000);
 }
